f2f1f593b19def2246c5a9f3abec52760f69d28b
Juan Ricardo Deossa <110882230+JuanDeossa@users.noreply.github.com>
2024-01-07 23:55:26 +0000

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <>
-      App
+      <Button variant="default" className="w-full bg-indigo-700">1</Button>
+      <Button variant="destructive" className="w-1/2">2</Button>
+      <Button variant="ghost" className="w-1/3">3</Button>
+      <Button variant="link" className="w-1/4">4</Button>
+      <Button variant="outline" className="w-1/5">4</Button>
+      <Button variant="secondary" className="w-1/6">4</Button>
     </>
   )
 }
